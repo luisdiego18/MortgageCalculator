1695238ba14ffb275920b7566e8286dfeca6f17c
luisdiego18 <diegotapia1987@gmail.com>
2022-11-14 16:04:19 -0600

    fix spacing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-            int principal = (int) Console.readNumber("Principal : ", 1000, 1_000_000);
-            float annualInterest = (float) Console.readNumber("Annual Interest Rate : ", 1, 30);
-            byte years = (byte) Console.readNumber("Period (Years) : ", 1, 30);
+            int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+            float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+            byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
             var calculator = new MortgageCalculator(principal, annualInterest, years);
 
